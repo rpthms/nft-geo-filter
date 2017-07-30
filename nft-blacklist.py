604c11d737ff5954130e070d558ff8d281bd3fa0
Ronnie P. Thomas <ronnietom@gmail.com>
2017-07-30 23:39:51 +0530

    Use a nft script instead of nft cli options
    Since there is a chance that the size of the IP list might cross
    the ARG_MAX limit, it is much better to add the IPs to a temporary
    nftables script and let nft handle the blacklist set

--- nft-blacklist.py
+++ nft-blacklist.py
@@ -4,13 +4,21 @@
 
 import urllib.request
 import subprocess
+import tempfile
 
 #Binaries
 NFT = '/usr/sbin/nft'
 
 #nftables Variables (Change accordingly)
+FAMILY = 'ip'
 TABLE = 'filter'
 SET_NAME = 'blacklist'
+
+#Temp File Header
+FILE_HEADER = ('table {} {} {{\n'
+               'set {} {{\n'
+               'type ipv4_addr\n'
+               'flags interval\n')
 
 IPDENY_URL= 'http://www.ipdeny.com/ipblocks/data/countries/{}.zone'
 COUNTRY_CODES = ('cn','ru')  #Check ipdeny.com for the country codes
@@ -22,11 +30,15 @@ def update_blacklist():
 
         print('Building list of IP blocks..')
         data = ip_blocks.read().decode('utf-8')
-        blacklist_ips = ','.join(data.splitlines())
-        blacklist_ips = ''.join(('{', blacklist_ips, '}'))
+        blacklist_ips = ',\n'.join(data.splitlines())
+        blacklist_ips = ''.join(('elements = {', blacklist_ips, '}\n}\n}\n'))
 
-        print('Adding the "{}" IP blocks to the blacklist..'.format(c))
-        subprocess.run([NFT, 'add element', TABLE, SET_NAME, blacklist_ips])
+        with tempfile.NamedTemporaryFile(mode='w') as tmp:
+            tmp.write(FILE_HEADER.format(FAMILY, TABLE, SET_NAME))
+            tmp.write(blacklist_ips)
+
+            print('Adding the "{}" IP blocks to the blacklist..'.format(c))
+            subprocess.run([NFT, '-f', tmp.name])
 
     print('Done!')
 
